8237005503d7afb8e57f58e31e16018249597166
许璐雪 <dew_sonw@126.com>
2019-06-13 17:38:26 +0800

    引擎升级4.3.0以后在Android8.0,9.0上文件打不开的问题

--- src/org/zywx/wbpalmstar/plugin/uexdocument/EUExDocumentReader.java
+++ src/org/zywx/wbpalmstar/plugin/uexdocument/EUExDocumentReader.java
@@ -1,25 +1,33 @@
 package org.zywx.wbpalmstar.plugin.uexdocument;
 
-import java.io.File;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+import android.widget.Toast;
 
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
 import org.zywx.wbpalmstar.engine.universalex.EUExUtil;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.widget.Toast;
+import java.io.File;
+import java.util.List;
+
+import static org.zywx.wbpalmstar.plugin.uexdocument.DocumentUtils.getMIMEType;
 
 public class EUExDocumentReader extends EUExBase {
 
 	public EUExDocumentReader(Context arg0, EBrowserView arg1) {
 		super(arg0, arg1);
 	}
+
+
 
 	public void openDocumentReader(String[] args) {
 		if (args == null || args.length < 1) {
@@ -54,16 +62,47 @@ public class EUExDocumentReader extends EUExBase {
 	        });
 			return;
 		}
+		openFile(mContext,file);
+//		Intent intent = new Intent();
+//		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//		String type = getMIMEType(file);
+//		Uri uri = Uri.fromFile(file);
+////		Uri photoURI = FileProvider.getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".provider",file);
+//		intent.setDataAndType(uri, type);
+//		try {
+//			intent.setAction(Intent.ACTION_VIEW);
+//			startActivity(Intent.createChooser(intent, null));
+//		} catch (ActivityNotFoundException e) {
+//			e.printStackTrace();
+//		}
+	}
 
-		Intent intent = new Intent();
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		String type = DocumentUtils.getMIMEType(file);
-		intent.setDataAndType(Uri.fromFile(file), type);
+	public  void openFile(Context context, File file){
 		try {
+			Intent intent = new Intent();
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			//设置intent的Action属性
 			intent.setAction(Intent.ACTION_VIEW);
-			startActivity(Intent.createChooser(intent, null));
-		} catch (ActivityNotFoundException e) {
+			//获取文件file的MIME类型
+			String type = getMIMEType(file);
+			//设置intent的data和Type属性。android 7.0以上crash,改用provider
+			if (Build.VERSION.SDK_INT >= 24) {
+				Uri fileUri = FileProvider.getUriForFile(context, context.getPackageName()+".provider", file);//android 7.0以上
+				intent.setDataAndType(fileUri, type);
+//				grantUriPermission(context, fileUri, intent);
+				List<ResolveInfo> resInfoList = context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+				for (ResolveInfo resolveInfo : resInfoList) {
+					String packageName = resolveInfo.activityInfo.packageName;
+					context.grantUriPermission(packageName, fileUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+				}
+			}else {
+				intent.setDataAndType(/*uri*/Uri.fromFile(file), type);
+			}
+			//跳转
+			context.startActivity(intent);
+		}catch (Exception e){
 			e.printStackTrace();
+			Toast.makeText(context, "File corrupted, download again please.", Toast.LENGTH_SHORT).show();
 		}
 
 	}
